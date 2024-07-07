8c165e4da73eab47f5bcb16c109384638e021980
Kacper Krawczyk <32519806+kaspher@users.noreply.github.com>
2024-07-07 20:52:31 +0200

    changed maps api library to MapBox (#33)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,10 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Icon } from 'leaflet';
-import 'leaflet/dist/leaflet.css';
 import Home from "./components/pages/Home";
 import NoPage from "./components/pages/NoPage";
 import Trails from "./components/pages/Trails";
 import Layout from "./components/pages/Layout";
 import Activities from "./components/pages/Activities";
-
-delete Icon.Default.prototype._getIconUrl;
-Icon.Default.mergeOptions({
-    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-    iconUrl: require('leaflet/dist/images/marker-icon.png'),
-    shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
-});
 
 function App() {
   return (
---
+++ frontend/src/components/trails/MapWithRoutes.js
@@ -0,0 +1,71 @@
+import React, {useEffect, useRef, useState} from 'react';
+import mapboxgl from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import '../../styles/Routes.css'
+import {fetchRoute} from "../../utils/trailsUtils";
+
+mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
+
+const MapWithRoutes = ({ trails }) => {
+    console.log('Mapbox Access Token:', process.env.REACT_APP_MAPBOX_ACCESS_TOKEN);
+    const mapContainerRef = useRef(null);
+    const [map, setMap] = useState(null);
+
+    useEffect(() => {
+        const initializeMap = ({ setMap, mapContainerRef }) => {
+            const map = new mapboxgl.Map({
+                container: mapContainerRef.current,
+                style: 'mapbox://styles/mapbox/streets-v11',
+                center: [19.9449799, 50.0646501],
+                zoom: 5,
+            });
+
+            map.on('load', async () => {
+                setMap(map);
+                map.resize();
+
+                for (const trail of trails) {
+                    const coordinates = trail.coordinates.map(coord => ({
+                        latitude: coord.latitude,
+                        longitude: coord.longitude
+                    }));
+
+                    const routeGeometry = await fetchRoute(coordinates);
+
+                    map.addSource(`route-${trail.id}`, {
+                        type: 'geojson',
+                        data: {
+                            type: 'Feature',
+                            properties: {},
+                            geometry: routeGeometry,
+                        },
+                    });
+
+                    map.addLayer({
+                        id: `route-${trail.id}`,
+                        type: 'line',
+                        source: `route-${trail.id}`,
+                        layout: {
+                            'line-join': 'round',
+                            'line-cap': 'round',
+                        },
+                        paint: {
+                            'line-color': '#0b6e12',
+                            'line-width': 5,
+                        },
+                    });
+                }
+            });
+        };
+
+        if (!map) initializeMap({ setMap, mapContainerRef });
+    }, [map, trails]);
+
+    return (
+        <div>
+            <div ref={mapContainerRef} className="map-with-routes"/>
+        </div>
+    );
+};
+
+export default MapWithRoutes;
