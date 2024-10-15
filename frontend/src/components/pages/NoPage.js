b971c95faa655be64ee1258f9b9e22692a9ab11e
Bezi <wojciech.bezak19@gmail.com>
2024-10-15 15:45:29 +0200

    404 page added

--- frontend/src/components/pages/NoPage.js
+++ frontend/src/components/pages/NoPage.js
@@ -1,5 +1,11 @@
 const NoPage = () => {
-    return <h1>404 :(</h1>;
+    return (
+        <div
+            className="flex items-center justify-center w-screen h-screen bg-cover bg-center bg-404"
+        >
+        </div>
+    );
 };
 
 export default NoPage;
+
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
     content: ['./src/**/*.{html,js,jsx}', './public/index.html'],
     theme: {
         extend: {},
+        backgroundImage:{
+            '404': "url(/src/assets/img/404_1.png)",
+        }
     },
     plugins: [require('@tailwindcss/forms')],
 }
