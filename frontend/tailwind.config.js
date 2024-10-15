1969bbdece6df0f009817df8b98e12e4efccf2cd
Bezi <wojciech.bezak19@gmail.com>
2024-10-15 19:05:02 +0200

    Made 404 page responsive

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,10 +2,13 @@
 module.exports = {
     content: ['./src/**/*.{html,js,jsx}', './public/index.html'],
     theme: {
-        extend: {},
-        backgroundImage:{
-            '404': "url(/src/assets/img/404_1.png)",
-        }
+        extend: {
+            backgroundImage: {
+                '404-sky': "url('/src/assets/img/404_sky.svg')",
+                '404-forest': "url('/src/assets/img/404_forest.svg')",
+                '404-background': "url('/src/assets/img/404_1.png')"
+            }
+        },
     },
     plugins: [require('@tailwindcss/forms')],
 }
