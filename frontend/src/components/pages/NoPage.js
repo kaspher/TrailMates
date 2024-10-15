9c0609c7f65e1e302d13b44b4bd42f94a81d2ba6
Bezi <147994263+bezi19@users.noreply.github.com>
2024-10-15 19:06:21 +0200

    404 page added (#38)
    * 404 page added
    
    * Made 404 page responsive

--- frontend/src/components/pages/NoPage.js
+++ frontend/src/components/pages/NoPage.js
@@ -1,5 +1,25 @@
 const NoPage = () => {
-    return <h1>404 :(</h1>;
+    return (
+        <div className="relative w-screen h-screen overflow-hidden">
+
+            {/* Las */}
+            <div className="absolute bottom-0 left-0 w-full h-full h-1/3 bg-404-forest bg-cover bg-bottom bg-no-repeat" />
+
+            {/* Tło nieba */}
+            <div className="absolute top-0 left-0 w-full h-2/3 bg-404-sky bg-cover bg-no-repeat" />
+            
+           {/* Tekst */}
+           <div className="absolute top-1/4 w-full text-center font-poppins">
+                <h1 className="text-9xl font-bold text-gray-800">
+                    404
+                </h1>
+                <h2 className="text-3xl font-semibold text-gray-800 mt-4">
+                    Ups! Wygląda na to że zabłądziłeś!
+                </h2>
+            </div>
+
+        </div>
+    );
 };
 
 export default NoPage;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
     content: ['./src/**/*.{html,js,jsx}', './public/index.html'],
     theme: {
-        extend: {},
+        extend: {
+            backgroundImage: {
+                '404-sky': "url('/src/assets/img/404_sky.svg')",
+                '404-forest': "url('/src/assets/img/404_forest.svg')",
+                '404-background': "url('/src/assets/img/404_1.png')"
+            }
+        },
     },
     plugins: [require('@tailwindcss/forms')],
 }
