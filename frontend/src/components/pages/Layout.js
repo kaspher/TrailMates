074e1d6e36462c7fbae41c17b47fd796abc7ac85
kaspher <kacperkrawczyk1@gmail.com>
2024-12-07 14:50:27 +0100

    adjusted and fixed layout mobile view

--- frontend/src/components/pages/Layout.js
+++ frontend/src/components/pages/Layout.js
@@ -8,6 +8,9 @@ import {
   faHome,
   faMap,
   faTasks,
+  faCalendarAlt,
+  faRunning,
+  faBars,
 } from "@fortawesome/free-solid-svg-icons";
 import logoUrl from "../../assets/img/logo/longlogo.svg";
 import mobileLogoUrl from "../../assets/img/logo/logo.svg";
@@ -64,130 +67,200 @@ const Layout = () => {
                 <img
                   src={isMobileView ? mobileLogoUrl : logoUrl}
                   alt="TrailMates"
-                  className={`w-auto filter brightness-0 ${isMobileView ? "h-8" : "h-5"}`}
+                  className={`w-auto filter brightness-0 ${
+                    isMobileView ? "h-8" : "h-5"
+                  }`}
                 />
               </Link>
             </div>
-
             {!isMobileView && (
-              <div className="hidden md:flex md:items-center md:justify-center md:gap-5">
-                <Link
-                  className="inline-block rounded-lg px-4 py-2 text-md font-medium text-gray-900 transition-all duration-200 hover:bg-gray-100 hover:text-gray-900"
-                  to="/"
-                >
-                  Strona domowa
-                </Link>
-                <Link
-                  className="inline-block rounded-lg px-4 py-2 text-md font-medium text-gray-900 transition-all duration-200 hover:bg-gray-100 hover:text-gray-900"
-                  to="/trails"
-                >
-                  Mapa tras
-                </Link>
-                <Link
-                  className="inline-block rounded-lg px-4 py-2 text-md font-medium text-gray-900 transition-all duration-200 hover:bg-gray-100 hover:text-gray-900"
-                  to="/activities"
-                >
-                  Aktywności
-                </Link>
-                <Link
-                  className="inline-block rounded-lg px-4 py-2 text-md font-medium text-gray-900 transition-all duration-200 hover:bg-gray-100 hover:text-gray-900"
-                  to="/events"
-                >
-                  Wydarzenia
-                </Link>
+              <div className="flex-1 flex justify-center">
+                <div className="flex items-center space-x-5">
+                  <Link
+                    className="inline-block rounded-lg px-4 py-2 text-md font-medium text-gray-900 transition-all duration-200 hover:bg-gray-100 hover:text-gray-900"
+                    to="/"
+                  >
+                    Strona domowa
+                  </Link>
+                  <Link
+                    className="inline-block rounded-lg px-4 py-2 text-md font-medium text-gray-900 transition-all duration-200 hover:bg-gray-100 hover:text-gray-900"
+                    to="/trails"
+                  >
+                    Mapa tras
+                  </Link>
+                  <Link
+                    className="inline-block rounded-lg px-4 py-2 text-md font-medium text-gray-900 transition-all duration-200 hover:bg-gray-100 hover:text-gray-900"
+                    to="/activities"
+                  >
+                    Aktywności
+                  </Link>
+                  <Link
+                    className="inline-block rounded-lg px-4 py-2 text-md font-medium text-gray-900 transition-all duration-200 hover:bg-gray-100 hover:text-gray-900"
+                    to="/events"
+                  >
+                    Wydarzenia
+                  </Link>
+                </div>
               </div>
             )}
-
-            <div className="flex items-center justify-end gap-4">
-              <div className="relative" ref={dropdownRef}>
-                <button
-                  onClick={toggleDropdown}
-                  className="flex items-center space-x-2"
-                >
-                  {user && (
+            <div className="flex items-center gap-4">
+              {isMobileView ? (
+                <div className="relative" ref={dropdownRef}>
+                  <button
+                    onClick={toggleDropdown}
+                    className="flex items-center space-x-2"
+                  >
+                    {user ? (
+                      <img
+                        src={cloudFrontDomainName + user.id}
+                        alt="user avatar"
+                        className="h-10 w-10 rounded-full object-cover object-center"
+                      />
+                    ) : (
+                      <FontAwesomeIcon
+                        icon={faBars}
+                        className="text-gray-900 text-2xl"
+                      />
+                    )}
+                  </button>
+                  {isDropdownOpen && (
+                    <div className="fixed top-full left-0 w-full bg-white shadow-lg ring-1 ring-black ring-opacity-5 z-20">
+                      <div className="py-1">
+                        <Link
+                          to="/"
+                          className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 w-full text-left"
+                          onClick={handleDropdownClose}
+                        >
+                          <FontAwesomeIcon icon={faHome} className="mr-2" />
+                          Strona domowa
+                        </Link>
+                        <Link
+                          to="/trails"
+                          className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 w-full text-left"
+                          onClick={handleDropdownClose}
+                        >
+                          <FontAwesomeIcon icon={faMap} className="mr-2" />
+                          Mapa tras
+                        </Link>
+                        <Link
+                          to="/activities"
+                          className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 w-full text-left"
+                          onClick={handleDropdownClose}
+                        >
+                          <FontAwesomeIcon icon={faRunning} className="mr-2" />
+                          Aktywności
+                        </Link>
+                        <Link
+                          to="/events"
+                          className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 w-full text-left"
+                          onClick={handleDropdownClose}
+                        >
+                          <FontAwesomeIcon
+                            icon={faCalendarAlt}
+                            className="mr-2"
+                          />
+                          Wydarzenia
+                        </Link>
+                        {!user && (
+                          <>
+                            <hr className="my-2 border-gray-500 border-t-2 mx-2" />
+                            <Link
+                              className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 w-full text-left flex items-center"
+                              to="/login"
+                              onClick={handleDropdownClose}
+                            >
+                              Zaloguj się
+                            </Link>
+                            <Link
+                              className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 w-full text-left flex items-center"
+                              to="/register"
+                              onClick={handleDropdownClose}
+                            >
+                              Zarejestruj się
+                            </Link>
+                          </>
+                        )}
+                        {user && (
+                          <>
+                            <hr className="my-2 border-gray-500 border-t-2 mx-2" />
+                            <Link
+                              to="/profile"
+                              className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 w-full text-left flex items-center"
+                              onClick={handleDropdownClose}
+                            >
+                              <FontAwesomeIcon icon={faUser} className="mr-2" />
+                              Mój profil
+                            </Link>
+                            <Link
+                              to="/activities"
+                              className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 w-full text-left flex items-center"
+                              onClick={handleDropdownClose}
+                            >
+                              <FontAwesomeIcon
+                                icon={faTasks}
+                                className="mr-2"
+                              />
+                              Twoje aktywności
+                            </Link>
+                            <button
+                              onClick={() => {
+                                logOut();
+                                handleDropdownClose();
+                              }}
+                              className="block px-4 py-2 text-sm text-red-600 hover:bg-gray-100 w-full text-left flex items-center"
+                            >
+                              <FontAwesomeIcon
+                                icon={faSignOutAlt}
+                                className="mr-2"
+                              />
+                              Wyloguj się
+                            </button>
+                          </>
+                        )}
+                      </div>
+                    </div>
+                  )}
+                </div>
+              ) : user ? (
+                <div className="relative" ref={dropdownRef}>
+                  <button
+                    onClick={toggleDropdown}
+                    className="flex items-center space-x-2"
+                  >
                     <span className="text-gray-900 font-medium hidden md:inline">
                       Witaj, {user.name}
                     </span>
-                  )}
-                  <img
-                    src={user ? cloudFrontDomainName + user.id : logoUrl}
-                    alt="users avatar"
-                    className="h-10 w-10 rounded-full object-cover object-center"
-                  />
-                </button>
-                {isDropdownOpen && (
-                  <div
-                    className={`${
-                      isMobileView
-                        ? "fixed top-full left-0 w-full rounded-none"
-                        : "absolute right-0 mt-2 w-48 rounded-md"
-                    } bg-white shadow-lg ring-1 ring-black ring-opacity-5 z-20`}
-                  >
-                    <div className="py-1">
-                      {isMobileView && (
-                        <>
-                          <Link
-                            to="/"
-                            className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 w-full text-left flex items-center"
-                            onClick={handleDropdownClose}
-                          >
-                            <FontAwesomeIcon icon={faHome} className="mr-2" />
-                            Strona domowa
-                          </Link>
-                          <Link
-                            to="/trails"
-                            className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 w-full text-left flex items-center"
-                            onClick={handleDropdownClose}
-                          >
-                            <FontAwesomeIcon icon={faMap} className="mr-2" />
-                            Mapa tras
-                          </Link>
-                          <Link
-                            to="/activities"
-                            className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 w-full text-left flex items-center"
-                            onClick={handleDropdownClose}
-                          >
-                            <FontAwesomeIcon icon={faTasks} className="mr-2" />
-                            Aktywności
-                          </Link>
-                          <Link
-                            to="/events"
-                            className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 w-full text-left flex items-center"
-                            onClick={handleDropdownClose}
-                          >
-                            <FontAwesomeIcon icon={faTasks} className="mr-2" />
-                            Wydarzenia
-                          </Link>
-                        </>
-                      )}
-                      {user && (
-                        <>
-                          <Link
-                            to="/profile"
-                            className="block mb-1 px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 w-full text-left flex items-center"
-                            onClick={handleDropdownClose}
-                          >
-                            <FontAwesomeIcon icon={faUser} className="mr-2" />
-                            Mój profil
-                          </Link>
-                          <Link
-                            to="/activities"
-                            className="block mb-1 px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 w-full text-left flex items-center"
-                            onClick={handleDropdownClose}
-                          >
-                            <FontAwesomeIcon icon={faTasks} className="mr-2" />
-                            Twoje aktywności
-                          </Link>
-                        </>
-                      )}
-                      <hr className="my-2 border-gray-500 border-t-2 mx-2" />
-                      {user ? (
+                    <img
+                      src={cloudFrontDomainName + user.id}
+                      alt="user avatar"
+                      className="h-10 w-10 rounded-full object-cover object-center"
+                    />
+                  </button>
+                  {isDropdownOpen && (
+                    <div className="absolute right-0 mt-2 w-48 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 z-20">
+                      <div className="py-1">
+                        <Link
+                          to="/profile"
+                          className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 w-full text-left flex items-center"
+                          onClick={handleDropdownClose}
+                        >
+                          <FontAwesomeIcon icon={faUser} className="mr-2" />
+                          Mój profil
+                        </Link>
+                        <Link
+                          to="/activities"
+                          className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 w-full text-left flex items-center"
+                          onClick={handleDropdownClose}
+                        >
+                          <FontAwesomeIcon icon={faTasks} className="mr-2" />
+                          Twoje aktywności
+                        </Link>
                         <button
                           onClick={() => {
                             logOut();
                             handleDropdownClose();
                           }}
-                          className="block mt-1 px-4 py-2 text-sm text-red-600 hover:bg-gray-100 w-full text-left flex items-center"
+                          className="block px-4 py-2 text-sm text-red-600 hover:bg-gray-100 w-full text-left flex items-center"
                         >
                           <FontAwesomeIcon
                             icon={faSignOutAlt}
@@ -195,28 +268,26 @@ const Layout = () => {
                           />
                           Wyloguj się
                         </button>
-                      ) : (
-                        <>
-                          <Link
-                            className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 w-full text-left flex items-center"
-                            to="/register"
-                            onClick={handleDropdownClose}
-                          >
-                            Zarejestruj się
-                          </Link>
-                          <Link
-                            className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 w-full text-left flex items-center"
-                            to="/login"
-                            onClick={handleDropdownClose}
-                          >
-                            Zaloguj się
-                          </Link>
-                        </>
-                      )}
+                      </div>
                     </div>
-                  </div>
-                )}
-              </div>
+                  )}
+                </div>
+              ) : (
+                <div className="flex space-x-5">
+                  <Link
+                    className="inline-block rounded-lg border-2 border-primary bg-primary px-4 py-2 text-md font-medium text-white transition-all duration-200"
+                    to="/login"
+                  >
+                    Zaloguj się
+                  </Link>
+                  <Link
+                    className="inline-block rounded-lg border-2 border-primary px-4 py-2 text-md font-medium text-primary transition-all duration-200 hover:bg-gray-200"
+                    to="/register"
+                  >
+                    Zarejestruj się
+                  </Link>
+                </div>
+              )}
             </div>
           </div>
         </div>
