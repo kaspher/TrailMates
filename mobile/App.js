26fbbb3a35e7530c77a2e64e909a20c10fcc9363
Bezi <147994263+bezi19@users.noreply.github.com>
2024-12-28 13:59:39 +0100

    Added recording and saving trails data (#54)

--- mobile/App.js
+++ mobile/App.js
@@ -4,20 +4,16 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Login from './components/screens/Login';
 import Register from './components/screens/Register';
-import MapboxGL from '@rnmapbox/maps';
-import {PUBLIC_MAPBOX_ACCESS_TOKEN} from '@env';
 import TabNavigator from './components/utils/TabNavigator';
 import ForgotPassword from './components/screens/ForgotPassword';
 import Splash from './components/screens/Splash';
 import UserProfile from './components/screens/UserProfile';
 
-const Stack = createNativeStackNavigator();
 
-MapboxGL.setAccessToken(PUBLIC_MAPBOX_ACCESS_TOKEN);
+const Stack = createNativeStackNavigator();
 
 
 export default function App() {
-  console.log(PUBLIC_MAPBOX_ACCESS_TOKEN)
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Splash" options={{ headerShown: false }} >
