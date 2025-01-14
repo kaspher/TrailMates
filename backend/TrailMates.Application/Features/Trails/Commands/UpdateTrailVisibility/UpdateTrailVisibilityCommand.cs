29abd9b702a6d84b591e4e351d561ac6f898cda3
kaspher <kacperkrawczyk1@gmail.com>
2025-01-14 19:19:52 +0100

    added update trail visibility endpoint

---
+++ backend/TrailMates.Application/Features/Trails/Commands/UpdateTrailVisibility/UpdateTrailVisibilityCommand.cs
@@ -0,0 +1,26 @@
+﻿using CSharpFunctionalExtensions;
+using TrailMates.Application.Abstractions.Repositories;
+using TrailMates.Application.Mediator;
+using TrailMates.Domain.Errors;
+
+namespace TrailMates.Application.Features.Trails.Commands.UpdateTrailVisibility;
+
+public readonly record struct UpdateTrailVisibilityCommand(Guid TrailId)
+    : ICommand<UnitResult<Error>>;
+
+internal sealed class UpdateTrailVisibilityCommandHandler(ITrailRepository trailRepository)
+    : ICommandHandler<UpdateTrailVisibilityCommand, UnitResult<Error>>
+{
+    public async Task<UnitResult<Error>> Handle(
+        UpdateTrailVisibilityCommand command,
+        CancellationToken cancellationToken
+    )
+    {
+        var visibilityUpdateResult = await trailRepository.UpdateVisibility(command.TrailId);
+
+        if (visibilityUpdateResult.IsFailure)
+            return visibilityUpdateResult.ConvertFailure<UnitResult<Error>>();
+
+        return UnitResult.Success<Error>();
+    }
+}
